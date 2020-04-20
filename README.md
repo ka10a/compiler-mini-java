2bef92faeb3ac4479232b24c9e1eb994a0a0e093
Boris Zhestiankin <zhestyankin.ba@phystech.edu>
2020-04-20 23:00:43 +0300

    Add build script and update README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# compiler-mini-java
+# MiniJava Compiler
+
+To build project and compile a file from `samples` folder:
+
+```
+./tool.sh filename.java
+```
+
+It will also draw an AST image and save it to `graph.png`. 
---
+++ tool.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ "$#" -gt 1 ]; then
+    echo "Only one argument is allowed (filename.java)"
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "No filename specified"
+    exit 1
+fi
+
+PATH_TO_JAVA_FILE=../samples/$1
+
+mkdir build
+cd build
+cmake ..
+make compiler-mini-java
+dot -Tpng src/parser.gv -o ../graph.png
+./compiler-mini-java < "$PATH_TO_JAVA_FILE"
