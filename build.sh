9235027b4d5f797b22af8778b950f94727b0fa11
Boris Zhestiankin <zhestyankin.ba@phystech.edu>
2020-05-11 18:58:41 +0300

    add clang-tidy

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cmake . -B build -DCMAKE_BUILD_TYPE=Release
+cmake . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build -j 2 -t compiler-mini-java
