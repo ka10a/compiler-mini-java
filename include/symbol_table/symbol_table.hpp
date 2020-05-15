bcec5459be870f58b2a6ae383cf498a5566f3e65
Boris Zhestiankin <zhestyankin.ba@phystech.edu>
2020-05-15 18:41:14 +0300

    add SymbolTable class

---
+++ include/symbol_table/symbol_table.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string_view>
+#include <unordered_map>
+
+#include <ast/class_declaration.hpp>
+#include <ast/main_class.hpp>
+#include <symbol_table/class_info.hpp>
+
+class SymbolTable {
+public:
+    SymbolTable() = default;
+
+    ClassInfoPtr& GetOrdinaryClass(std::string_view class_name);
+    const ClassInfoPtr& GetOrdinaryClass(std::string_view class_name) const;
+    const ClassInfoPtr& GetMainClass() const;
+
+    bool HasClass(std::string_view class_name) const;
+
+    SymbolTable& AddMainClass(const MainClassPtr& main_class);
+    SymbolTable& AddOrdinaryClass(const ClassDeclarationPtr& class_declaration);
+
+private:
+    ClassInfoPtr main_class_;
+    std::unordered_map<std::string_view, ClassInfoPtr> classes_;
+};
---
+++ src/symbol_table/symbol_table.cpp
@@ -0,0 +1,33 @@
+#include <symbol_table/symbol_table.hpp>
+
+ClassInfoPtr& SymbolTable::GetOrdinaryClass(std::string_view class_name) {
+    return classes_[class_name];
+}
+
+const ClassInfoPtr& SymbolTable::GetOrdinaryClass(std::string_view class_name) const {
+    return classes_.at(class_name);
+}
+
+const ClassInfoPtr& SymbolTable::GetMainClass() const {
+    return main_class_;
+}
+
+bool SymbolTable::HasClass(std::string_view class_name) const {
+    return classes_.find(class_name) != classes_.end();
+}
+
+SymbolTable& SymbolTable::AddMainClass(const MainClassPtr& main_class) {
+    std::string_view class_name = main_class->GetClassName()->GetName();
+    main_class_ = std::make_shared<ClassInfo>(class_name, main_class->GetLocation());
+    return *this;
+}
+
+SymbolTable& SymbolTable::AddOrdinaryClass(const ClassDeclarationPtr& class_decl) {
+    std::string_view class_name = class_decl->GetClassName()->GetName();
+    classes_[class_name] =
+        class_decl->GetExtendsClassName()
+            ? std::make_shared<ClassInfo>(class_name, class_decl->GetLocation(),
+                                          class_decl->GetExtendsClassName()->GetName())
+            : std::make_shared<ClassInfo>(class_name, class_decl->GetLocation());
+    return *this;
+}
